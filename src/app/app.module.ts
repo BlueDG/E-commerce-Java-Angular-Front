90086ad9f4d8cdb639813d6e376f93f3b77c0ea4
amicius <admin@amicius.fr>
2019-02-26 14:45:54 +0100

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,23 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     NgbModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
     BrowserModule
   ],
   providers: [],
