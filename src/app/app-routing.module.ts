bb446dcbb98b3cd5e80126874a40dcec16291879
amicius <admin@amicius.fr>
2019-02-27 16:18:45 +0100

    correction doublon menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
-import { AppComponent } from 'src/app/app.component'
 import { AdminRechercheComponent } from './admin-recherche/admin-recherche.component';
 
 const routes: Routes = [
   { path: 'menu', component: MenuComponent },
-  { path: '', component: AppComponent },
   { path: 'login', component: LoginComponent },
   { path: 'search', component: AdminRechercheComponent },
   { path: '**', redirectTo: '' }
