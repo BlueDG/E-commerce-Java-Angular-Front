99e923905702592ac1fbad586f7701e10088f679
amicius <admin@amicius.fr>
2019-03-01 14:14:17 +0100

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -42,7 +42,7 @@ export class LoginComponent implements OnInit {
     ).subscribe(() => {
       this.successMessage = null;
       this.modalService.dismissAll();
-      this.router.navigateByUrl("/menu")
+      window.location.reload();
     });
     this._fail.subscribe((message) => this.failMessage = message);
     this._fail.pipe(
