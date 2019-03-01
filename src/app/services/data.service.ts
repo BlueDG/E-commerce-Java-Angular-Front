cbdfe73f0e8d5bcf591a4b0dacfcc851ab0828a5
bpayanpsl <bpayanpsl@gmail.com>
2019-03-01 09:45:45 +0100

    push

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -28,7 +28,7 @@ export class DataService {
 
 
   creerPostGame(postGame: PostGame) {
-    return this._http.get<PostGame>(`${environment.backendUrl}/game`);
+    return this._http.post<PostGame>(`${environment.backendUrl}/game`, postGame, httpOptions);
   }
 
 
