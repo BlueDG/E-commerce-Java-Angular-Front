a459622c04b2813597f75bb8dcc345f55ba434bf
Guillaume DG <Guillaume.dusseuxgicquel@gmail.com>
2019-03-01 17:14:27 +0100

    slider travail

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-accueil',
-  templateUrl: './accueil.component.html',
-  styleUrls: ['./accueil.component.css']
+  templateUrl: './accueil.component.html', 
+  styleUrls: ['./accueil.component.css'],
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
 })
-export class AccueilComponent implements OnInit {
 
-  constructor() { }
+export class AccueilComponent implements OnInit {
+  images = [1, 2, 3].map(() => `src/assets/img`);
+
+  constructor(config: NgbCarouselConfig) { 
+    // customize default values of carousels used by this component tree
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
 
   ngOnInit() {
   }
 
 }
+
+
