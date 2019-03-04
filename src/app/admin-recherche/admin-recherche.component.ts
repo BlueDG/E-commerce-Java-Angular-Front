75e723eb79cc78841af487758b2c300b653fca0c
amicius <admin@amicius.fr>
2019-03-04 10:27:48 +0100

    modification diverse sur recherche admin

--- src/app/admin-recherche/admin-recherche.component.ts
+++ src/app/admin-recherche/admin-recherche.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { PostGame } from 'src/models';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-recherche',
@@ -10,28 +12,36 @@ import { PostGame } from 'src/models';
 export class AdminRechercheComponent implements OnInit {
 
   postGame: PostGame = {};
-  message: string = "";
   games: PostGame[] = [];
   pages: number;
+
+  private _fail = new Subject<string>();
+  failMessage: string;
 
   constructor(private _data: DataService) { }
 
   ngOnInit() {
     this.games = [];
+    this._fail.subscribe((message) => this.failMessage = message);
+    this._fail.pipe(
+      debounceTime(5000)
+    ).subscribe(() => this.failMessage = null);
   }
 
   submit() {
     this._data.creerPostGame("1", this.postGame).subscribe(
       value => {
-        this.message = 'Enregistrement réussi.';
         this.games = value.games;
         this.pages = value.pages;
         console.log(value.games);
       },
-      error => { this.message = "Erreur lors de l'enregistrement." }
+      error => { this.changeFailMessage("Erreur lors de l'enregistrement."); }
     );
   }
 
+  public changeFailMessage(msg: string) {
+    this._fail.next(msg);
+  }
 
 }
 
