9b9ef7b828699e847c3812ce21ebb2d8b9d9e759
bpayanpsl <bpayanpsl@gmail.com>
2019-03-05 16:24:44 +0100

    produit page

--- src/app/produit-page/produit-page.component.ts
+++ src/app/produit-page/produit-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models';
 import { ActivatedRoute } from '@angular/router';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-produit-page',
@@ -10,11 +11,13 @@ import { ActivatedRoute } from '@angular/router';
 export class ProduitPageComponent implements OnInit {
 
   gameToShow : Game;
+  date : string;
   constructor(private _route: ActivatedRoute) { }
 
   ngOnInit() {
     this._route.queryParams.subscribe(params => {
       this.gameToShow = JSON.parse(params.toShow) as Game;
+      this.date = moment(this.gameToShow.date).format('MM/DD/YYYY');
     })
   }
 
